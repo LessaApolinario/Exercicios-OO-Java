a50bd53735ceb3fb4b8733f0495ab604d84db6ee
Lessa Apolinario <lessaapolinario22@outlook.com>
2021-11-24 23:21:05 -0300

    Implementando ArrayList para armazenar os objetos

--- OO/01 - aula/27 - exercicio/src/ContaDemo.java
+++ OO/01 - aula/27 - exercicio/src/ContaDemo.java
@@ -1,52 +1,61 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class ContaDemo {
 
   public static void main(String[] args) {
     Conta conta = new Conta("", 1, 0);
-    Conta[] contas = new Conta[1];
-
-    for (int i = 0; i < contas.length; i++) {
-      contas[i] = new Conta("", 1, 0);
-    }
-
+    ArrayList<Conta> contas = new ArrayList<Conta>();
     Scanner input = new Scanner(System.in);
+
     int op = -1;
-    int i = 0;
 
     do {
+      conta.menu();
       System.out.println("Escolha uma opção: ");
       op = input.nextInt();
+      limparBuffer(input);
 
       switch (op) {
         case 1:
-          conta.cadastrarCliente();
+          conta.cadastrarCliente(contas);
+          break;
+        case 2:
+          System.out.println("Qual é o nome do cliente? ");
+          String nomeCliente = input.nextLine();
+          conta.consultarCliente(nomeCliente, contas);
+          break;
+        case 3:
+          System.out.println("Qual é o nome do cliente? ");
+          nomeCliente = input.nextLine();
+          conta.verificarSaldo(nomeCliente, contas);
+          break;
+        case 4:
+          System.out.println("Qual é o nome do cliente? ");
+          nomeCliente = input.nextLine();
 
-          try {
-            contas[i] =
-              new Conta(
-                conta.getNome(),
-                conta.getNumeroConta(),
-                conta.getSaldo()
-              );
-          } catch (ArrayIndexOutOfBoundsException exception) {
-            Conta[] novaConta = new Conta[contas.length + 1];
+          System.out.println("Qual a quantia a ser sacada? ");
+          double quantia = input.nextDouble();
+          conta.efetuarSaque(quantia, contas, nomeCliente);
+          break;
+        case 5:
+          System.out.println("Qual é o nome do cliente? ");
+          nomeCliente = input.nextLine();
 
-            System.arraycopy(contas, 0, novaConta, 0, contas.length);
-
-            contas = novaConta;
-            contas[i] =
-              new Conta(
-                conta.getNome(),
-                conta.getNumeroConta(),
-                conta.getSaldo()
-              );
-          } finally {
-            System.out.println("Cliente cadastrado com sucesso!");
-            i++;
-          }
+          System.out.println("Qual a quantia a ser depositada? ");
+          quantia = input.nextDouble();
+          conta.efetuarDeposito(quantia, contas, nomeCliente);
+          break;
+        case 0:
+          System.out.println("Você saiu!");
           break;
       }
     } while (op != 0);
+  }
+
+  public static void limparBuffer(Scanner sc) {
+    if (sc.hasNextLine()) {
+      sc.nextLine();
+    }
   }
 }

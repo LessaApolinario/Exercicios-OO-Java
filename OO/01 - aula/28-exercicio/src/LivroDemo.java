88aa233fee1815b2afbe48c0496c8bafd74b397c
Lessa Apolinario <lessaapolinario@gmail.com>
2022-01-20 14:07:49 -0300

    Add loop and menu to the main class

--- OO/01 - aula/28-exercicio/src/LivroDemo.java
+++ OO/01 - aula/28-exercicio/src/LivroDemo.java
@@ -1,14 +1,33 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class LivroDemo {
 
   public static void main(String[] args) {
-    Livro livro = new Livro("Augusto Santos", "A", "12345678-9", "Legal", 12);
+    Livro livro = new Livro("", "", "12345678-9", "", 1);
+    ArrayList<Livro> livros = new ArrayList<Livro>();
+    Scanner input = new Scanner(System.in);
 
-    System.out.println(
-      livro.getAutor() +
-      livro.getEditora() +
-      livro.getISBN() +
-      livro.getTitulo() +
-      livro.getAnoEdicao()
-    );
+    int op = -1;
+
+    do {
+      Livro.menu();
+      System.out.println("Escolha uma opção: ");
+      op = input.nextInt();
+      limparBuffer(input);
+
+      switch (op) {
+        case 1:
+          System.out.println("Bem-vindo(a) ao cadastro de livros");
+          livro.cadastrarLivro(livros);
+          break;
+      }
+    } while (op != 0);
+  }
+
+  public static void limparBuffer(Scanner sc) {
+    if (sc.hasNextLine()) {
+      sc.nextLine();
+    }
   }
 }

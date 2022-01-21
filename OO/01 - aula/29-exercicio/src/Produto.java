8901ba4292b24d7dcc22a635ebc34e6b3b9808e6
Lessa Apolinario <lessaapolinario@gmail.com>
2022-01-21 13:29:48 -0300

    Add constructor, Getters and Setters

---
+++ OO/01 - aula/29-exercicio/src/Produto.java
@@ -0,0 +1,61 @@
+public class Produto {
+
+  private String nome;
+  private int codigo;
+  private double preco;
+  private int quantidade;
+
+  public Produto(String nome, int codigo, double preco, int quantidade) {
+    this.nome = nome;
+
+    if (codigo >= 0) {
+      this.codigo = codigo;
+    }
+
+    if (preco > 0) {
+      this.preco = preco;
+    }
+
+    if (quantidade > 0) {
+      this.quantidade = quantidade;
+    }
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
+  public String getNome() {
+    return nome;
+  }
+
+  public void setCodigo(int codigo) {
+    if (codigo >= 0) {
+      this.codigo = codigo;
+    }
+  }
+
+  public int getCodigo() {
+    return codigo;
+  }
+
+  public void setPreco(double preco) {
+    if (preco > 0) {
+      this.preco = preco;
+    }
+  }
+
+  public double getPreco() {
+    return preco;
+  }
+
+  public void setQuantidade(int quantidade) {
+    if (quantidade > 0) {
+      this.quantidade = quantidade;
+    }
+  }
+
+  public int getQuantidade() {
+    return quantidade;
+  }
+}

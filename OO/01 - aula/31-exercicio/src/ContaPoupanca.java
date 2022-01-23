4d555f2f98942b958b46bc159f26143c51511509
Lessa Apolinario <lessaapolinario@gmail.com>
2022-01-23 18:53:04 -0300

    Add ContaPoupanca subclass and its methods

---
+++ OO/01 - aula/31-exercicio/src/ContaPoupanca.java
@@ -0,0 +1,14 @@
+public class ContaPoupanca extends Conta {
+
+    public ContaPoupanca(int numeroConta, double taxa, double saldo, String banco) {
+        super(numeroConta, taxa, saldo, banco);
+    }
+
+    // Adiciona uma quantia ao saldo, que por sua vez é uma porcentagem!
+    public double atualizar(double taxa) {
+        double saldo = super.getSaldo();
+        saldo += saldo * (taxa / 100);
+
+        return saldo;
+    }
+}

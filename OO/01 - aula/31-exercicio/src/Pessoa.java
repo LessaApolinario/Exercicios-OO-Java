766f8a313a65f09e83d5297a8c41356c492f3ae2
Lessa Apolinario <lessaapolinario@gmail.com>
2022-01-23 18:39:58 -0300

    Add superclass Pessoa and its methods

---
+++ OO/01 - aula/31-exercicio/src/Pessoa.java
@@ -0,0 +1,51 @@
+public class Pessoa {
+
+    private String nome;
+    private String cpf;
+    private String dataNascimento;
+
+    public Pessoa(String nome, String cpf, String dataNascimento) {
+        if (!nome.equals("")) {
+            this.nome = nome;
+        }
+
+        if (!cpf.equals("") && cpf.length() == 14) {
+            this.cpf = cpf;
+        }
+
+        if (!dataNascimento.equals("")) {
+            this.dataNascimento = dataNascimento;
+        }
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        if (!nome.equals("")) {
+            this.nome = nome;
+        }
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        if (!cpf.equals("") && cpf.length() == 14) {
+            this.cpf = cpf;
+        }
+    }
+
+    public String getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(String dataNascimento) {
+        if (!dataNascimento.equals("")) {
+            this.dataNascimento = dataNascimento;
+        }
+    }
+}
+

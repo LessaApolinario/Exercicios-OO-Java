d4e42bb7e66a6f312676e9726612402148cab9bf
Lessa Apolinario <lessaapolinario@gmail.com>
2022-01-22 23:28:27 -0300

    Add cadastrarAluno and cadastrarProfessor

--- OO/01 - aula/30-exercicio/src/GerenciadorDePessoas.java
+++ OO/01 - aula/30-exercicio/src/GerenciadorDePessoas.java
@@ -18,4 +18,19 @@ public class GerenciadorDePessoas {
     public ArrayList<Professor> getProfessores() {
         return professores;
     }
+
+    // Cadastrando um aluno, observe que usamos também os atributos de pessoa
+    public Aluno cadastrarAluno(String nome, String cpf, String dataNascimento, double nota1, double nota2,
+            double nota3, double nota4) {
+        Aluno aluno = new Aluno(nome, cpf, dataNascimento, nota1, nota2, nota3, nota4);
+
+        return aluno;
+    }
+
+    // Cadastrando um professor
+    public Professor cadastrarProfessor(String nome, String cpf, String dataNascimento, double salario) {
+        Professor professor = new Professor(nome, cpf, dataNascimento, salario);
+
+        return professor;
+    }
 }

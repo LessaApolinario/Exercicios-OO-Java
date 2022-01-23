7f93d63970f86ee6a197a9eea7435f932a5d483b
Lessa Apolinario <lessaapolinario@gmail.com>
2022-01-23 18:49:36 -0300

    Add ContaCorrente subclass and its methods

---
+++ OO/01 - aula/31-exercicio/src/ContaCorrente.java
@@ -0,0 +1,14 @@
+public class ContaCorrente extends Conta {
+
+    public ContaCorrente(int numeroConta, double taxa, double saldo, String banco) {
+        super(numeroConta, taxa, saldo, banco);
+    }
+
+    // Subtrai uma quantia do saldo, que por sua vez é uma porcentagem!
+    public double atualizar(double taxa) {
+        double saldo = super.getSaldo();
+        saldo -= saldo * (taxa / 100);
+
+        return saldo;
+    }
+}

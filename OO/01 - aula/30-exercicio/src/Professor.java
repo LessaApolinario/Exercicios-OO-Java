b68481ae325e3e2194db3b71a5fc4c5234330504
Lessa Apolinario <lessaapolinario@gmail.com>
2022-01-22 23:34:09 -0300

    Add obterMediaAluno method

--- OO/01 - aula/30-exercicio/src/Professor.java
+++ OO/01 - aula/30-exercicio/src/Professor.java
@@ -19,4 +19,11 @@ public class Professor extends Pessoa {
             this.salario = salario;
         }
     }
+
+    // O professor é quem calcula as notas dos alunos e mostra a média!
+    public String obterMediaAluno(Aluno aluno) {
+        double mediaAluno = (aluno.getNota1() + aluno.getNota2() + aluno.getNota3() + aluno.getNota4()) / 4;
+
+        return "A média do aluno " + aluno.getNome() + " é: " + mediaAluno;
+    }
 }

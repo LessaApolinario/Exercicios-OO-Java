e95c95d9abc871416b0d29530d5135307d3c5491
Lessa Apolinario <lessaapolinario@gmail.com>
2022-02-04 21:28:50 -0300

    Add class Funcionario and its methods

---
+++ OO/01 - aula/32-exercicio/src/funcionarios/Funcionario.java
@@ -0,0 +1,67 @@
+package funcionarios;
+
+public class Funcionario {
+
+    private String nome;
+    private String cpf;
+    private double salario;
+    private String dataNascimento;
+
+    public Funcionario(String nome, String cpf, double salario, String dataNascimento) {
+        if (!nome.equals("")) {
+            this.nome = nome;
+        }
+
+        if (!cpf.equals("") && cpf.length() == 14) {
+            this.cpf = cpf;
+        }
+
+        if (salario > 0) {
+            this.salario = salario;
+        }
+
+        if (!dataNascimento.equals("")) {
+            this.dataNascimento = dataNascimento;
+        }
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        if (!nome.equals("")) {
+            this.nome = nome;
+        }
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        if (!cpf.equals("") && cpf.length() == 14) {
+            this.cpf = cpf;
+        }
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        if (salario > 0) {
+            this.salario = salario;
+        }
+    }
+
+    public String getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(String dataNascimento) {
+        if (!dataNascimento.equals("")) {
+            this.dataNascimento = dataNascimento;
+        }
+    }
+}

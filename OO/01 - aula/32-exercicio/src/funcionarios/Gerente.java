c271b29426abc26b366acbc8cb7b052d71774822
Lessa Apolinario <lessaapolinario@gmail.com>
2022-02-04 21:31:41 -0300

    Add Gerente subclass

---
+++ OO/01 - aula/32-exercicio/src/funcionarios/Gerente.java
@@ -0,0 +1,15 @@
+package funcionarios;
+
+public class Gerente extends Funcionario {
+
+    public Gerente(String nome, String cpf, double salario, String dataNascimento) {
+        super(nome, cpf, salario, dataNascimento);
+    }
+
+    public double calcularSalario() {
+        double salario = super.getSalario();
+        salario += 2000;
+
+        return salario;
+    }
+}

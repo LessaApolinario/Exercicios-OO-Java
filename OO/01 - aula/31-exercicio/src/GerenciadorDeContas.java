e6760207e787274d48e17d0ba1d79c9d8d78ea82
Lessa Apolinario <lessaapolinario@gmail.com>
2022-02-05 21:01:47 -0300

    Add senha in abrirContaPoupanca abrirContaCorrente

--- OO/01 - aula/31-exercicio/src/GerenciadorDeContas.java
+++ OO/01 - aula/31-exercicio/src/GerenciadorDeContas.java
@@ -1,13 +1,13 @@
 public class GerenciadorDeContas {
 
-    public ContaCorrente abrirContaCorrente(int numeroConta, double taxa, double saldo, String banco) {
-        ContaCorrente cc = new ContaCorrente(numeroConta, taxa, saldo, banco);
+    public ContaCorrente abrirContaCorrente(String senha, int numeroConta, double taxa, double saldo, String banco) {
+        ContaCorrente cc = new ContaCorrente(senha, numeroConta, taxa, saldo, banco);
 
         return cc;
     }
 
-    public ContaPoupanca abrirContaPoupanca(int numeroConta, double taxa, double saldo, String banco) {
-        ContaPoupanca cp = new ContaPoupanca(numeroConta, taxa, saldo, banco);
+    public ContaPoupanca abrirContaPoupanca(String senha, int numeroConta, double taxa, double saldo, String banco) {
+        ContaPoupanca cp = new ContaPoupanca(senha, numeroConta, taxa, saldo, banco);
 
         return cp;
     }

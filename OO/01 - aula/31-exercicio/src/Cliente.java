5467d16791bd99b83ac26918940707e7c368a250
Lessa Apolinario <lessaapolinario@gmail.com>
2022-02-05 21:06:15 -0300

    Remove rules for cpf and Conta subclasses objects from constructor

--- OO/01 - aula/31-exercicio/src/Cliente.java
+++ OO/01 - aula/31-exercicio/src/Cliente.java
@@ -8,17 +8,7 @@ public class Cliente {
             this.nome = nome;
         }
 
-        if (!cpf.equals("") && cpf.length() == 14) {
-            this.cpf = cpf;
-        }
-
-        if (contaCorrente != null) {
-            this.contaCorrente = contaCorrente;
-        }
-
-        if (contaPoupanca != null) {
-            this.contaPoupanca = contaPoupanca;
-        }
+        this.cpf = cpf;
     }
 
     public String getNome() {
@@ -36,24 +26,26 @@ public class Cliente {
     public void setCpf(String cpf) {
         this.cpf = cpf;
     }
-/*
-    public void addContaPoupanca(ContaPoupanca conta) {
-        contas.add(conta)
-    }
-
-    public void addContaCorrente(ContaCorrente conta) {
-        contas.add(conta)
-    }
-
-    public ArrayList<ContaPoupanca> getContasPoupancas () {
-        ArrayList <ContaPoupanca> contasPoupanca = new ArrayList<>();
-
-        for (Conta conta : contas) {
-            if (conta instanceof ContaPoupanca) {
-                contasPoupanca.add(conta)
-            }
-        }
-
-        return contasPoupanca
-    }*/
+    /// TODO: implement these methodss
+    /*
+     * public void addContaPoupanca(ContaPoupanca conta) {
+     * contas.add(conta)
+     * }
+     *
+     * public void addContaCorrente(ContaCorrente conta) {
+     * contas.add(conta)
+     * }
+     *
+     * public ArrayList<ContaPoupanca> getContasPoupancas () {
+     * ArrayList <ContaPoupanca> contasPoupanca = new ArrayList<>();
+     *
+     * for (Conta conta : contas) {
+     * if (conta instanceof ContaPoupanca) {
+     * contasPoupanca.add(conta)
+     * }
+     * }
+     *
+     * return contasPoupanca
+     * }
+     */
 }

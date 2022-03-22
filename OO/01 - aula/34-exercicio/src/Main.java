d794558aed7866b58179b04ee1a68b93eb1964e7
Lessa Apolinario <lessaapolinario@gmail.com>
2022-03-22 20:09:42 -0300

    Add cadastrar log and static gerenciadorDeGerentes

--- OO/01 - aula/34-exercicio/src/Main.java
+++ OO/01 - aula/34-exercicio/src/Main.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Main {
 
     static GerenciadorDeFuncionarios gerenciadorDeFuncionarios = new GerenciadorDeFuncionarios();
+    static GerenciadorDeGerentes gerenciadorDeGerentes = new GerenciadorDeGerentes();
 
     public static void main(String[] args) {
 
@@ -10,10 +11,6 @@ public class Main {
 
     public static void abrirDialogCadastarFuncionario() {
         Scanner in = new Scanner(System.in);
-
-        System.out.println(
-            "String nome, String cpf, String dataNascimento, double salario"
-        );
 
         System.out.println("Bem-vindo ao cadastro de funcionários!");
         System.out.println("Nome: ");
@@ -30,5 +27,28 @@ public class Main {
 
         gerenciadorDeFuncionarios.cadastrarFuncionario(nome, cpf, dataNascimento, salario);
         System.out.println("Funcionário cadastrado com sucesso!");
+    }
+
+    public static void abrirDialogCadastarGerente() {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Bem-vindo ao cadastro de gerentes!");
+        System.out.println("Nome: ");
+        String nome = in.nextLine();
+
+        System.out.println("cpf: ");
+        String cpf = in.nextLine();
+
+        System.out.println("Data de nascimento: ");
+        String dataNascimento = in.nextLine();
+
+        System.out.println("Salário: ");
+        double salario = in.nextDouble();
+
+        System.out.println("Departamento: ");
+        String departamento = in.nextLine();
+
+        gerenciadorDeGerentes.cadastrarGerente(nome, cpf, dataNascimento, salario, departamento);
+        System.out.println("Gerente cadastrado com sucesso!");
     }
 }

2978788ac36af8620b1a089b813d7313c62d8813
Lessa Apolinario <lessaapolinario@gmail.com>
2022-03-30 20:43:55 -0300

    Add GerenciadorDeTrabalhadorHorista class and its methods

---
+++ OO/01 - aula/35-exercicio/src/com/lessaapolinario/managers/GerenciadorDeTrabalhadorHorista.java
@@ -0,0 +1,35 @@
+package com.lessaapolinario.managers;
+
+import com.lessaapolinario.classes.TrabalhadorHorista;
+import com.lessaapolinario.exceptions.TrabalhadorNaoEncontradoException;
+import com.lessaapolinario.interfaces.ManipulaTrabalhadorHorista;
+
+import java.util.ArrayList;
+
+public class GerenciadorDeTrabalhadorHorista implements ManipulaTrabalhadorHorista {
+    private ArrayList<TrabalhadorHorista> trabalhadores;
+
+    public GerenciadorDeTrabalhadorHorista() {
+        this.trabalhadores = new ArrayList<>();
+    }
+
+    @Override
+    public TrabalhadorHorista cadastrarTrabalhadorHorista(String nome, String cadastro, double salario, int idade, String endereco, int totalHorasTrabalhar, double salarioHora) {
+        TrabalhadorHorista th = new TrabalhadorHorista(nome, cadastro, salario, idade, endereco, totalHorasTrabalhar, salarioHora);
+        trabalhadores.add(th);
+        return th;
+    }
+
+    @Override
+    public TrabalhadorHorista buscarTrabalhadorHorista(String cadastro) throws TrabalhadorNaoEncontradoException {
+        for (TrabalhadorHorista th : trabalhadores) {
+            String cadastroHorista = th.getCadastro();
+
+            if (cadastroHorista.equals(cadastro)) {
+                return th;
+            }
+        }
+
+        return null;
+    }
+}

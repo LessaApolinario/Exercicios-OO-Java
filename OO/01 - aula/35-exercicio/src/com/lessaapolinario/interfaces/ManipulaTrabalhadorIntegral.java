c52639de9f1c262cbdec139b48a2f5d74dd75d13
Lessa Apolinario <lessaapolinario@gmail.com>
2022-03-30 20:30:27 -0300

    Add ManipulaTrabalhadorIntegral interface

---
+++ OO/01 - aula/35-exercicio/src/com/lessaapolinario/interfaces/ManipulaTrabalhadorIntegral.java
@@ -0,0 +1,10 @@
+package com.lessaapolinario.interfaces;
+
+import com.lessaapolinario.classes.TrabalhadorIntegral;
+
+public interface ManipulaTrabalhadorIntegral {
+
+    TrabalhadorIntegral cadastrarTrabalhadorIntegral(String nome, String cadastro, double salario, int idade, String endereco);
+
+    TrabalhadorIntegral buscarTrabalhadorIntegral(String cadastro);
+}

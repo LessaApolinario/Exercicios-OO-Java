e6fb1ba424eaafb0df3a39cda0ac79be1c2942d5
Lessa Apolinario <lessaapolinario@gmail.com>
2022-03-30 20:12:41 -0300

    Add new getSalario method for TrabalhadorIntegral class

--- OO/01 - aula/35-exercicio/src/com/lessaapolinario/classes/TrabalhadorIntegral.java
+++ OO/01 - aula/35-exercicio/src/com/lessaapolinario/classes/TrabalhadorIntegral.java
@@ -5,4 +5,22 @@ public class TrabalhadorIntegral extends Trabalhador {
     public TrabalhadorIntegral(String nome, String cadastro, double salario, int idade, String endereco) {
         super(nome, cadastro, salario, idade, endereco);
     }
+
+    @Override
+    public double getSalario() {
+        double salario = super.getSalario();
+        int idade = super.getIdade();
+        /*
+            Para trabalhadores até 30 anos,
+            o aumento é de 16%, caso contrário
+            é 25%.
+        */
+        if (idade <= 30) {
+            salario += salario * 0.16;
+        } else {
+            salario += salario * 0.25;
+        }
+
+        return salario;
+    }
 }

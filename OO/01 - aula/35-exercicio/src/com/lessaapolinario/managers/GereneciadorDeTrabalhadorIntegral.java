58380edb5d7c3419bdf48611b96541ccd869989a
Lessa Apolinario <lessaapolinario@gmail.com>
2022-03-31 09:33:03 -0300

    Add Add GerenciadorDeTrabalhadorIntegral class and its methods

---
+++ OO/01 - aula/35-exercicio/src/com/lessaapolinario/managers/GereneciadorDeTrabalhadorIntegral.java
@@ -0,0 +1,34 @@
+package com.lessaapolinario.managers;
+
+import com.lessaapolinario.classes.TrabalhadorIntegral;
+import com.lessaapolinario.interfaces.ManipulaTrabalhadorIntegral;
+
+import java.util.ArrayList;
+
+public class GereneciadorDeTrabalhadorIntegral implements ManipulaTrabalhadorIntegral {
+    private ArrayList<TrabalhadorIntegral> trabalhadores;
+
+    public GereneciadorDeTrabalhadorIntegral() {
+        this.trabalhadores = new ArrayList<>();
+    }
+
+    @Override
+    public TrabalhadorIntegral cadastrarTrabalhadorIntegral(String nome, String cadastro, double salario, int idade, String endereco) {
+        TrabalhadorIntegral ti = new TrabalhadorIntegral(nome, cadastro, salario, idade, endereco);
+        trabalhadores.add(ti);
+        return ti;
+    }
+
+    @Override
+    public TrabalhadorIntegral buscarTrabalhadorIntegral(String cadastro) {
+        for (TrabalhadorIntegral ti : trabalhadores) {
+            String cadastroIntegral = ti.getCadastro();
+
+            if (cadastroIntegral.equals(cadastro)) {
+                return ti;
+            }
+        }
+
+        return null;
+    }
+}
